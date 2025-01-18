384fb8175f7eccc26aeba476c57b96445e174e28
Tsedenawit <tsedenawit3@gmail.com>
2025-01-18 11:51:16 -0500

    updated

--- quiz.js
+++ quiz.js
@@ -1,9 +1,10 @@
 function checkAnswer(){
    let correctAnswer = "4"; 
    const userAnswer = document.querySelector('input[name="quiz"]:checked')?.value;
-   if (userAnswer == correctAnswer) {
+   if (userAnswer === correctAnswer) {
     document.getElementById("feedback").textContent="Correct! Well done.";
   } else {
     document.getElementById("feedback").textContent="That's incorrect. Try again!";
   }
+
 }
