935db714de89dcb02cc2bd37f97a1891ec418ca2
Tsedenawit <tsedenawit3@gmail.com>
2025-01-18 11:42:06 -0500

    send

--- quiz.js
+++ quiz.js
@@ -1,4 +1,9 @@
 function checkAnswer(){
    let correctAnswer = "4"; 
-   
+   let answer = document.getElementsByName("quiz");
+   if (answer) {
+    console.log("User's answer:", answer);
+  } else {
+    console.log("No option selected.");
+  }
 }
