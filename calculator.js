41c68e9c192aadf9d88c5cbd1c39983598a59f86
Tsedenawit <tsedenawit3@gmail.com>
2025-01-18 11:58:47 -0500

    s

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
 function add(number1, number2) {
     return number1 + number2;
 }
+function subtract(number1, number2) {
+    return number1 + number2;
+}
+function multiply(number1, number2) {
+    return number1 + number2;
+}
